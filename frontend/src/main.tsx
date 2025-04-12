b260401085e22dd0bfec11359272f12a99435c87
yashjain0313 <yashjain0313@gmail.com>
2025-04-12 17:51:03 +0530

    themeS

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+// Force the theme to be "black"
+document.documentElement.setAttribute("data-theme", "black");
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
-    }
-  }
+    },
+  },
 });
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient} >
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
     </QueryClientProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
